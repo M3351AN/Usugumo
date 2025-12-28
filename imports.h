f91eaf9d4c18a80cf0f7628cb92caba0d9d967d2
渟雲 <QuQ@outlook.it>
2025-12-29 02:59:47 +0800

    chore: remove unused import

--- imports.h
+++ imports.h
@@ -11,12 +11,7 @@ NTKERNELAPI NTSTATUS
 IoCreateDriver(_In_ PUNICODE_STRING DriverName,
                _In_ PDRIVER_INITIALIZE InitializationFunction);
 
-__declspec(dllimport) NTSTATUS MmCopyVirtualMemory(
-    IN PEPROCESS FromProcess, IN CONST VOID* FromAddress,
-    IN PEPROCESS ToProcess, OUT PVOID ToAddress, IN SIZE_T BufferSize,
-    IN KPROCESSOR_MODE PreviousMode, OUT PSIZE_T NumberOfBytesCopied);
-
-__declspec(dllimport) PPEB PsGetProcessPeb(PEPROCESS);
+NTSYSAPI PPEB PsGetProcessPeb(PEPROCESS);
 
 NTSYSCALLAPI
 POBJECT_TYPE* IoDriverObjectType;
@@ -29,6 +24,7 @@ ObReferenceObjectByName(__in PUNICODE_STRING ObjectName, __in ULONG Attributes,
                         __in POBJECT_TYPE ObjectType,
                         __in KPROCESSOR_MODE AccessMode,
                         __inout_opt PVOID ParseContext, __out PVOID* Object);
+
 NTSYSAPI
 NTSTATUS
 NTAPI
