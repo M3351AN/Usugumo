4fa2e033457a225c2c6faae7180c17e9ac2db015
渟雲 <QuQ@outlook.it>
2026-01-01 08:51:30 +0800

    chore: declare kmemmove __cdecl

--- reimpl.h
+++ reimpl.h
@@ -7,7 +7,7 @@ int __cdecl kstricmp(const char* Str1, const char* Str2);
 
 int __cdecl kwcsicmp(const wchar_t* Str1, const wchar_t* Str2);
 
-void* kmemmove(void* dst, const void* src, size_t count);
+void* __cdecl kmemmove(void* dst, const void* src, size_t count);
 
 NTSTATUS
 DriverCopyVirtualMemory(IN PEPROCESS SourceProcess, IN PVOID SourceAddress,
