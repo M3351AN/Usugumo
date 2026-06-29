693ceab6dd669e348a59e42a7184b9727f8853b0
渟雲 <QuQ@outlook.it>
2026-06-30 04:29:35 +0800

    chore: instancelize input injection

--- includes/keybd_input_injection.h
+++ includes/keybd_input_injection.h
@@ -2,51 +2,62 @@
 #pragma once
 #ifndef _KEYBD_INPUT_INJECTION_H_
 #define _KEYBD_INPUT_INJECTION_H_
-#include <Windows.h>
 
+#include <Windows.h>
 #include <iostream>
 
 class KeyboardInterface {
- private:
-  using NtUserInjectKeyboardInput_t = void(WINAPI*)(KEYBDINPUT*, int);
-  NtUserInjectKeyboardInput_t nt_user_inject_keyboard_input_ = nullptr;
+private:
+    using NtUserInjectKeyboardInput_t = void (WINAPI*)(KEYBDINPUT*, int);
+    NtUserInjectKeyboardInput_t nt_user_inject_keyboard_input_ = nullptr;
+    bool initialized_ = false;
 
- public:
-  KeyboardInterface() {
-    LoadLibraryW(L"user32.dll");
-    HMODULE win32u_module = LoadLibraryW(L"win32u.dll");
-    if (!win32u_module) {
-      std::cerr << "[-] Could not load win32u.dll\n";
-      return;
+    KeyboardInterface() {
+        HMODULE win32u_module = LoadLibraryW(L"win32u.dll");
+        if (win32u_module) {
+            nt_user_inject_keyboard_input_ =
+                reinterpret_cast<NtUserInjectKeyboardInput_t>(
+                    GetProcAddress(win32u_module, "NtUserInjectKeyboardInput"));
+            if (nt_user_inject_keyboard_input_) {
+                initialized_ = true;
+            } else {
+                std::cerr << "[-] Could not locate NtUserInjectKeyboardInput\n";
+            }
+        } else {
+            std::cerr << "[-] Could not load win32u.dll\n";
+        }
     }
-    nt_user_inject_keyboard_input_ =
-        reinterpret_cast<NtUserInjectKeyboardInput_t>(
-            GetProcAddress(win32u_module, "NtUserInjectKeyboardInput"));
-    if (!nt_user_inject_keyboard_input_) {
-      std::cerr << "[-] Could not locate NtUserInjectKeyboardInput\n";
-    }
-  }
 
-  inline bool sendEvent(const KEYBDINPUT& info) {
-    if (nt_user_inject_keyboard_input_) {
-      nt_user_inject_keyboard_input_(const_cast<KEYBDINPUT*>(&info), 1);
-      return true;
+public:
+    KeyboardInterface(const KeyboardInterface&) = delete;
+    KeyboardInterface& operator=(const KeyboardInterface&) = delete;
+    KeyboardInterface(KeyboardInterface&&) = delete;
+    KeyboardInterface& operator=(KeyboardInterface&&) = delete;
+
+    static KeyboardInterface& getInstance() {
+        static KeyboardInterface instance;
+        return instance;
     }
-    return false;
-  }
+
+    inline bool sendEvent(const KEYBDINPUT& info) {
+        if (initialized_ && nt_user_inject_keyboard_input_) {
+            nt_user_inject_keyboard_input_(const_cast<KEYBDINPUT*>(&info), 1);
+            return true;
+        }
+        return false;
+    }
 };
-
+namespace {
 inline void my_keybd_event(BYTE vk, BYTE scan, DWORD flags,
                            ULONG_PTR dw_extra_info) {
-  static KeyboardInterface keyboard_interface;
+    KEYBDINPUT event = {};
+    event.wVk = vk;
+    event.wScan = scan;
+    event.dwFlags = flags;
+    event.time = 0;
+    event.dwExtraInfo = dw_extra_info;
 
-  KEYBDINPUT event = {};
-  event.wVk = vk;
-  event.wScan = scan;
-  event.dwFlags = flags;
-  event.time = 0;
-  event.dwExtraInfo = dw_extra_info;
-
-  keyboard_interface.sendEvent(event);
+    KeyboardInterface::getInstance().sendEvent(event);
 }
-#endif
+}
+#endif // _KEYBD_INPUT_INJECTION_H_
--- includes/mouse_input_injection.h
+++ includes/mouse_input_injection.h
@@ -2,6 +2,7 @@
 #pragma once
 #ifndef _MOUSE_INPUT_INJECTION_H_
 #define _MOUSE_INPUT_INJECTION_H_
+
 #include <Windows.h>
 #include <iostream>
 
@@ -9,36 +10,46 @@ class MouseInterface {
 private:
     using NtUserInjectMouseInput_t = bool (*)(MOUSEINPUT*, int);
     NtUserInjectMouseInput_t nt_user_inject_mouse_input_ = nullptr;
+    bool initialized_ = false;
 
-public:
     MouseInterface() {
-        LoadLibraryW(L"user32.dll");
         HMODULE win32u_module = LoadLibraryW(L"win32u.dll");
-        if (!win32u_module) {
+        if (win32u_module) {
+            nt_user_inject_mouse_input_ =
+                reinterpret_cast<NtUserInjectMouseInput_t>(
+                    GetProcAddress(win32u_module, "NtUserInjectMouseInput"));
+            if (nt_user_inject_mouse_input_) {
+                initialized_ = true;
+            } else {
+                std::cerr << "[-] Could not locate NtUserInjectMouseInput\n";
+            }
+        } else {
             std::cerr << "[-] Could not load win32u.dll\n";
-            return;
-        }
-        nt_user_inject_mouse_input_ =
-            reinterpret_cast<NtUserInjectMouseInput_t>(
-                GetProcAddress(win32u_module, "NtUserInjectMouseInput"));
-        if (!nt_user_inject_mouse_input_) {
-            std::cerr << "[-] Could not locate NtUserInjectMouseInput\n";
         }
     }
 
+public:
+    MouseInterface(const MouseInterface&) = delete;
+    MouseInterface& operator=(const MouseInterface&) = delete;
+    MouseInterface(MouseInterface&&) = delete;
+    MouseInterface& operator=(MouseInterface&&) = delete;
+
+    static MouseInterface& getInstance() {
+        static MouseInterface instance;
+        return instance;
+    }
+
     inline bool sendEvent(const MOUSEINPUT& info) {
-        if (nt_user_inject_mouse_input_) {
+        if (initialized_ && nt_user_inject_mouse_input_) {
             return nt_user_inject_mouse_input_(
                 const_cast<MOUSEINPUT*>(&info), 1);
         }
         return false;
     }
 };
-
+namespace {
 inline void my_mouse_event(DWORD dw_flags, LONG dx, LONG dy, DWORD dw_data,
                            ULONG_PTR dw_extra_info) {
-    static MouseInterface mouse_interface;
-
     MOUSEINPUT event = {};
     event.dx = dx;
     event.dy = dy;
@@ -47,6 +58,7 @@ inline void my_mouse_event(DWORD dw_flags, LONG dx, LONG dy, DWORD dw_data,
     event.time = 0;
     event.dwExtraInfo = dw_extra_info;
 
-    mouse_interface.sendEvent(event);
+    MouseInterface::getInstance().sendEvent(event);
 }
-#endif
+}
+#endif // _MOUSE_INPUT_INJECTION_H_
